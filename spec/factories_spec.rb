7af8d1a0cafe9d996851ff7ea059968a54805940
James Simshaw <james.simshaw@gmail.com>
2016-11-04 19:23:16 -0700

    Added commenting to factories_spec

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# Test all of our factories to verify they are valid
 FactoryGirl.factories.map(&:name).each do |factory|
   describe "The #{factory} factory" do
     it "is valid" do
